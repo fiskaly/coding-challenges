f7325c04ee1db1d51cfa1f51c9fccc4208463179
stefan.kohlbacher <bb9a3f4ecc360c7911919fb4b5414cf239a01735>
2021-12-02 21:01:49 +0100

    install knex and pg; fix welcome message in react app

--- typescript-crm-challenge/frontend/src/helloWorld.js
+++ typescript-crm-challenge/frontend/src/helloWorld.js
@@ -1,19 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
 import config from './config';
 
 
 export default function HelloWorld(props) {
-  let text = 'loading...';
+  const [text, setText] = useState('loading...');
 
   const url = `${config.BACKEND_URL}:${config.BACKEND_PORT}/hello`;
   axios.get(url)
     .then((res) => {
       console.log(res.data);
       if (res.status !== 200) {
-        text = `${res.status} error when calling the backend`;
+        setText(`${res.status} error when calling the backend`);
       } else {
-        text = res.data;
+        setText(res.data);
       }
     });
 
