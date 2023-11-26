06946f88ec84d35fc4f1023b26eee11348bb7ccb
Andrei Sferdian <andrei.sferdian@opswat.com>
2023-11-26 18:26:29 +0200

    add SignatureDevice struct and constructor

--- signing-service-challenge-go/crypto/signer.go
+++ signing-service-challenge-go/crypto/signer.go
@@ -1,8 +1,41 @@
 package crypto
 
+import (
+	"errors"
+)
+
+const (
+	AlgorithmRSA = "RSA"
+	AlgorithmECC = "ECC"
+)
+
+func NewSigner(algorithm string) (Signer, error) {
+	switch algorithm {
+	case AlgorithmRSA:
+		return &RSASigner{}, nil
+	case AlgorithmECC:
+		return &ECCSigner{}, nil
+	default:
+		return nil, errors.New("invalid algorithm")
+	}
+}
+
 // Signer defines a contract for different types of signing implementations.
 type Signer interface {
 	Sign(dataToBeSigned []byte) ([]byte, error)
 }
 
 // TODO: implement RSA and ECDSA signing ...
+type RSASigner struct {
+}
+
+func (signer *RSASigner) Sign(dataToBeSigned []byte) ([]byte, error) {
+	return nil, errors.New("not implemented")
+}
+
+type ECCSigner struct {
+}
+
+func (signer *ECCSigner) Sign(dataToBeSigned []byte) ([]byte, error) {
+	return nil, errors.New("not implemented")
+}
--- signing-service-challenge-go/domain/device.go
+++ signing-service-challenge-go/domain/device.go
@@ -1,3 +1,25 @@
 package domain
 
-// TODO: signature device domain model ...
+import (
+	"github.com/fiskaly/coding-challenges/signing-service-challenge/crypto"
+	"github.com/google/uuid"
+)
+
+type SignatureDevice struct {
+	Id     uuid.UUID
+	Label  *string
+	signer crypto.Signer
+}
+
+func NewSignatureDevice(id uuid.UUID, label *string, algorithm string) (*SignatureDevice, error) {
+	signer, err := crypto.NewSigner(algorithm)
+	if err != nil {
+		return nil, err
+	}
+
+	return &SignatureDevice{
+		Id:     id,
+		Label:  label,
+		signer: signer,
+	}, nil
+}
