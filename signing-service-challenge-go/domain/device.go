e3f94b76842296ff52567d98e8ab2bf91db0d88d
Andrei Sferdian <andrei.sferdian@opswat.com>
2023-11-26 20:10:54 +0200

    add logic to sign data

--- signing-service-challenge-go/domain/device.go
+++ signing-service-challenge-go/domain/device.go
@@ -1,14 +1,24 @@
 package domain
 
 import (
+	"encoding/base64"
+	"fmt"
+
 	"github.com/fiskaly/coding-challenges/signing-service-challenge/crypto"
 	"github.com/google/uuid"
 )
 
 type SignatureDevice struct {
-	Id     uuid.UUID
-	Label  *string
-	signer crypto.Signer
+	Id               uuid.UUID
+	Label            *string
+	signer           crypto.Signer
+	signatureCounter uint
+	lastSignature    string
+}
+
+type SignDataResult struct {
+	Signature  []byte
+	SignedData []byte
 }
 
 func NewSignatureDevice(id uuid.UUID, label *string, algorithm string) (*SignatureDevice, error) {
@@ -17,9 +27,39 @@ func NewSignatureDevice(id uuid.UUID, label *string, algorithm string) (*Signatu
 		return nil, err
 	}
 
+	lastSignature := base64.StdEncoding.EncodeToString([]byte(id.String()))
+
 	return &SignatureDevice{
-		Id:     id,
-		Label:  label,
-		signer: signer,
+		Id:               id,
+		Label:            label,
+		signer:           signer,
+		signatureCounter: 0,
+		lastSignature:    lastSignature,
 	}, nil
+}
+
+func (device *SignatureDevice) Sign(dataToBeSigned []byte) (*SignDataResult, error) {
+	securedData := device.secureData(dataToBeSigned)
+
+	signature, err := device.signer.Sign(securedData)
+	if err != nil {
+		return nil, err
+	}
+
+	// TODO: make thread safe
+	device.signatureCounter += 1
+	device.lastSignature = base64.StdEncoding.EncodeToString(signature)
+
+	return &SignDataResult{
+		Signature:  signature,
+		SignedData: securedData,
+	}, nil
+}
+
+func (device *SignatureDevice) secureData(dataToBeSigned []byte) []byte {
+	sigCounter := []byte(fmt.Sprintf("%d_", device.signatureCounter))
+	lastSig := []byte(fmt.Sprintf("_%s", device.lastSignature))
+	securedData := append(append(sigCounter, dataToBeSigned...), lastSig...)
+
+	return securedData
 }
---
+++ signing-service-challenge-go/domain/device_test.go
@@ -0,0 +1,43 @@
+package domain_test
+
+import (
+	"encoding/base64"
+	"fmt"
+	"testing"
+
+	"github.com/fiskaly/coding-challenges/signing-service-challenge/crypto"
+	"github.com/fiskaly/coding-challenges/signing-service-challenge/domain"
+	"github.com/google/uuid"
+)
+
+func TestSignWithRSA(t *testing.T) {
+	id := uuid.New()
+	device, err := domain.NewSignatureDevice(id, nil, crypto.AlgorithmECC)
+	if err != nil {
+		t.Fatal("failed to create signature device")
+	}
+
+	data := "data_to_be_signed"
+	actual, err := device.Sign([]byte(data))
+	if err != nil {
+		t.Fatal(err)
+	}
+	lastSignature := base64.StdEncoding.EncodeToString(actual.Signature)
+
+	expectedSignedData := fmt.Sprintf("%d_%s_%s", 0, data, base64.StdEncoding.EncodeToString([]byte(id.String())))
+	t.Logf("expected - %s", expectedSignedData)
+	if expectedSignedData != string(actual.SignedData) {
+		t.Fatalf("expected - %s | actual - %s", expectedSignedData, actual.SignedData)
+	}
+
+	actual, err = device.Sign([]byte(data))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expectedSignedData = fmt.Sprintf("%d_%s_%s", 1, data, lastSignature)
+	t.Logf("expected - %s", expectedSignedData)
+	if expectedSignedData != string(actual.SignedData) {
+		t.Fatalf("expected - %s | actual - %s", expectedSignedData, actual.SignedData)
+	}
+}
